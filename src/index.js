fb9340a3feb8f524ae6e981a8398c2e5d419b23f
Anna Shulyak <ourmac@Ours-iMac.fios-router.home>
2022-05-02 17:06:50 -0400

    pushing js code to get data, edit dog and update dog in db

--- src/index.js
+++ src/index.js
@@ -1,3 +1,59 @@
-document.addEventListener('DOMContentLoaded', () => {
+const dataUrl = "http://localhost:3000/dogs"
 
+function renderDogs() {
+    fetch(`${dataUrl}`).then(res=> res.json()).then(data=>{
+        console.log(data)
+        data.forEach(dog => {
+            const tBody = document.querySelector("#table-body")
+            const tr = document.createElement('tr')
+            const tdName = document.createElement('td')
+            tdName.textContent = dog.name
+            const tdBreed = document.createElement('td')
+            tdBreed.textContent = dog.breed
+            const tdSex = document.createElement('td')
+            tdSex.textContent = dog.sex
+            const tdEdit = document.createElement('td')
+            const editBtn = document.createElement('button')
+            editBtn.className = "edit-btn"
+            editBtn.id = dog.id
+            editBtn.textContent='Edit'
+            tdEdit.append(editBtn)
+            tr.append(tdName, tdBreed, tdSex, tdEdit)
+            editBtn.addEventListener('click', (e) => {
+                console.log(e)
+                const form = document.querySelector('#dog-form')
+                const editForm = document.querySelectorAll("form input")
+                console.log(editForm[3])
+                editForm[0].value = dog.name
+                editForm[1].value = dog.breed
+                editForm[2].value = dog.sex
+                form.addEventListener('submit', ( (e)=> {
+                    e.preventDefault()
+                    console.log(e)
+                    const dogObj = {
+                        name: e.target[0].value,
+                        breed: e.target[1].value,
+                        sex: e.target[2].value
+                    }
+                    console.log(dogObj)
+                    console.log(`${dataUrl}/${dog.id}`)
+                    fetch(`${dataUrl}/${dog.id}`, {
+                        method: "PATCH",
+                        headers: {
+                            "Content-type":"application/json",
+                            "Accept": "application/json"
+                        },
+                        body: JSON.stringify(dogObj)}).
+                        then(res => res.json()).then(data => console.log(data))
+                }))
+            })
+            tBody.append(tr)
+        })
+     })
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('DOM Loaded')
+    renderDogs()
 })
